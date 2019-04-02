a16d85a40c58ac5b8fb5e895c3c33f093d95327d
Donovan Schafroth <donovanschafroth@gmail.com>
2019-04-01 22:22:15 -0500

    buttons populating and html/css mostly layed out

--- assets/logic.js
+++ assets/logic.js
@@ -1,1 +1,22 @@
-alert("another test");
+$(document).ready(function() {
+
+    var words = [
+        "dog", "cat", "hamster", "turtle", "bird", "teacup pig",
+        "snake", "bear", "salamander", "frog", "ferret", "sugar glider",
+        "skunk", "goldfish", "penguin", "owl", "bearded dragon", "rabbit"
+    ];
+
+    function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+        $(areaToAddTo).empty();
+
+        for (var i=0; i < arrayToUse.length; i++) {
+            var a = $("<button>");
+            a.addClass(classToAdd);
+            a.attr("data-type", arrayToUse[i]);
+            a.addClass("btn ml-1 btn-outline-success");
+            a.text(arrayToUse[i]);
+            $(areaToAddTo).append(a);
+        }
+    }
+    populateButtons(words, "words-button", "#gifButtons");
+});
